e01d592a2646bfbd458e09f22b8747e0d0ba9d97
JC-MT <jancarlosmatias@gmail.com>
2022-07-26 11:51:40 -0400

    adds error handling, balance-color, and loading spinner logic

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -10,21 +10,45 @@ function Index() {
       .get(`${API}/transactions`)
       .then((res) => {
         setTransactions(res.data);
+        document.getElementById('spinner').className = 'invisible';
       })
       .catch((err) => {
-        console.warn(err);
+        console.warn(err.message);
+        document.getElementById('status').innerText = err.message
+        document.getElementById('spinner').className = 'invisible'
       });
-
   }, []);
-  
+
+  const spinner = (
+    <div id="spinner" className="flex items-center justify-center">
+      <div className="w-10 h-10 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+    </div>
+  );
+
+  let accountTotal = (
+    <span id="balance">
+      {transactions.reduce((acc, num) => {
+        acc += Number(num.amount);
+        return acc;
+      }, 0)}
+    </span>
+  );
+
+  let getColorForBalance = (accountTotal) => {
+    if (accountTotal.props.children > 1000) {
+      document.getElementById('balance').className = 'text-lime-700';
+    } else if (accountTotal.props.children < 0) {
+      document.getElementById('balance').className = 'text-red-600';
+    }
+    return accountTotal;
+  };
+
   return (
     <div>
-      <h2 className="text-[50px] text-center">
-        Bank Account Total: ${(transactions ? transactions.reduce((acc, num) => {
-          acc += Number(num.amount)
-          return acc;
-        }, 0) : 0)}
+      <h2 id="status" className="text-[50px] text-center">
+        Bank Account Total: ${getColorForBalance(accountTotal)}
       </h2>
+      {spinner}
       <Transactions transactions={transactions} />
     </div>
   );
